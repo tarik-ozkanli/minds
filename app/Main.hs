c5b9310083aefb59bc03a655f5da0f9cfc883a64
tarik-ozkanli <tozkanli2023@gmail.com>
2021-01-11 18:19:35 +0300

    appending routes in the same process

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,8 @@ import WordRoutes
 
 import Web.Scotty
 
+
+
 main :: IO ()
-main = do 
-  scotty 3000 dayRoutes
-  scotty 3001 wordRoutes
-        
+main = do
+  scotty 3000 (dayRoutes >> wordRoutes)
